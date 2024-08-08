eebc03580223ec9878fcee71df889795054b9b16
Aman singh baghel <141499645+itsaman22@users.noreply.github.com>
2024-08-09 00:13:58 +0530

    script.js

--- script.js
+++ script.js
@@ -3,29 +3,52 @@ function highlightGridItems() {
     const filter = input.value.toLowerCase();
     const gridItems = document.querySelectorAll('.grid-item');
     const originalColor = '#004276'; 
-    const highlightColor = '#35b6b4'; 
+    const highlightColor = '#0082A8'; 
+    let highlightedItem = null;
+    
+
+    if (filter === '') {
+        
+        gridItems.forEach(item => {
+            item.style.backgroundColor = originalColor;
+            item.querySelector('a').style.color = 'white';
+        });
+        return; 
+    }
 
     gridItems.forEach(item => {
+        item.style.backgroundColor = originalColor;
+        item.querySelector('a').style.color = 'white';
+    });
+
+    let highlightCount = 0;
+    gridItems.forEach(item => {
         const name = item.getAttribute('data-name').toLowerCase();
-        if (filter === '') {
-            item.style.backgroundColor = originalColor;
-        } else if (name.includes(filter)) {
+        if (name.includes(filter)) {
             item.style.backgroundColor = highlightColor;
-        } else {
-            item.style.backgroundColor = originalColor;
+            item.querySelector('a').style.color = 'white';
+            highlightedItem = item;
+            highlightCount++;
         }
     });
+
+    if (highlightCount === 1) {
+        highlightedItem.classList.add('single-highlight');
+    } else {
+        gridItems.forEach(item => item.classList.remove('single-highlight'));
+    }
 }
 
+document.getElementById('search-input').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        const highlightedItem = document.querySelector('.grid-item.single-highlight');
+        if (highlightedItem) {
+            const link = highlightedItem.querySelector('a');
+            if (link) {
+                link.click(); 
+            }
+        }
+    }
+});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var resetButton = document.querySelector('.resetbtn');
-
-    var resetPasswordURL = 'https://selfheal.maxhealthcare.com/resetaccount/';
-  
-    resetButton.addEventListener('click', function() {
-      window.open(resetPasswordURL, '_blank');
-    });
-  });
-  
+document.getElementById('search-input').addEventListener('input', highlightGridItems);
